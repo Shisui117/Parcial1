0da743969e1336638fc65b52cfe223c044ba3b18
Alfredo Valentín Santa Ana Gaytan <60673921+Shisui117@users.noreply.github.com>
2020-02-06 16:32:28 -0600

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,22 @@
+var num1 =Math.floor(Math.random()*19+1);
+var num2=Math.floor(Math.random()*19+1);
+var num3=Math.floor(Math.random()*19+1);
+
+console.log("///Lista de numeros///");
+console.log("Primer numero :"+num1);
+console.log("Segundo numero :"+num2);
+console.log("Tercer numero :"+num3);
+
+if(num1>num2 && num1>num3){
+    
+    console.log("El primer numero es mayor");
+}
+else{
+    
+    if(num2>num1 && num2>num3){
+        console.log("El segundo numero es mayor");
+    }
+else{    
+        console.log("El tercer numero es mayor");   
+}
+}
